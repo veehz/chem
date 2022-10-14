9b93bd3756b53bf99fd7331753627e5e304c1b06
veehz <veehzhz@gmail.com>
2022-10-14 21:44:44 +0800

    clean code

--- preprocess.js
+++ preprocess.js
@@ -1,58 +1,66 @@
-const pp = require('preprocess');
+"use strict";
 
-const SOURCE = "src"
-const DEST = "docs"
+const pp = require("preprocess");
 
-let exclude = [
-    "/header.html"
-]
+const SOURCE = "src";
+const DEST = "docs";
 
-const path = require('path');
-exclude = exclude.map(e => path.join(SOURCE, e));
+let exclude = ["/header.html"];
+
+const path = require("path");
+exclude = exclude.map((e) => path.join(SOURCE, e));
 
 // recursively process all files in SOURCE and output to DESTINATION
 
-const fs = require('fs');
-
+const fs = require("fs");
 
 function processFile(file) {
-    if (exclude.includes(file)) {
-        return;
-    }
-    if(file.includes("__includes")){
-        return;
-    }
-    const source = fs.readFileSync(file, 'utf8');
-    let srcDir = file.substring(0, file.lastIndexOf('/'));
-    const processed = pp.preprocess(source, process.env, {
-        srcDir,
-    });
-    // mkdir recursively
-    const dest = file.replace(SOURCE, DEST);
-    const destDir = dest.substring(0, dest.lastIndexOf('/'));
-    fs.mkdirSync(destDir, { recursive: true });
+  if (exclude.includes(file)) {
+    return;
+  }
+  if (file.includes("__includes")) {
+    return;
+  }
+  let source = fs.readFileSync(file, "utf8");
+  let srcDir = file.substring(0, file.lastIndexOf("/"));
 
-    // if exists and same output, don't write
-    if (fs.existsSync(dest)) {
-        const destContent = fs.readFileSync(dest, 'utf8');
-        if (destContent === processed) {
-            return;
-        }
-    } 
+  // replace "@include $lib/" with relative path
+  if (source.includes("@include $lib/")) {
+    let relativePath = path.relative(srcDir, path.join(SOURCE, "lib"));
+    source = source.replace(/@include \$lib\//g, `@include ${relativePath}/`);
+  }
 
-    fs.writeFileSync(dest, processed);
+  const processed = pp.preprocess(source, process.env, {
+    srcDir,
+  });
+
+  const dest = file.replace(SOURCE, DEST);
+  // if exists and same output, don't write
+  if (fs.existsSync(dest)) {
+    const destContent = fs.readFileSync(dest, "utf8");
+    if (destContent === processed) {
+      return;
+    }
+  }
+
+  // mkdir recursively
+  const destDir = dest.substring(0, dest.lastIndexOf("/"));
+  fs.mkdirSync(destDir, { recursive: true });
+
+  fs.writeFileSync(dest, processed);
 }
 
 function processDir(dir) {
-    const files = fs.readdirSync(dir);
-    for (const file of files) {
-        const path = dir + '/' + file;
-        if (fs.statSync(path).isDirectory()) {
-            processDir(path);
-        } else {
-            processFile(path);
-        }
+  if(path.normalize(dir) == path.normalize(SOURCE + "/lib"))  return;
+  const files = fs.readdirSync(dir);
+  for (const file of files) {
+    const path = dir + "/" + file;
+    if (fs.statSync(path).isDirectory()) {
+      processDir(path);
+    } else {
+      processFile(path);
     }
+  }
 }
 
 processDir(SOURCE);
