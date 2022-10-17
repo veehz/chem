8bfa8ae5b92090c371e8904fc25bff1237bc55fd
veehz <veehzhz@gmail.com>
2022-10-17 17:45:21 +0800

    add content

---
+++ src/organic/vs/__includes/equations.js
@@ -0,0 +1,22 @@
+const equations = {
+    "3-2": {
+        title: String.raw`烯 加成 \(\ce{Br2/CCl4}\)`,
+        content: String.raw`\(\ce{H3CCH=CH2 + Br2 ->[\ce{CCl4}] H3CCHBrCH2Br}\)`
+    },
+    "3-3": {
+        title: String.raw`烯 氧化 \(\ce{KMnO4}\)`,
+        content: String.raw`
+            1. 冷稀碱性 \(\ce{KMnO4}\) 溶液 (紫色褪色)
+            \[\ce{3H2C=CH2 + 2KMnO4 + 4H2O ->[\ce{OH-}] 3CH2(OH)CH2(OH) + 2KOH + 2MnO2}\]
+            2. 冷稀酸性 \(\ce{KMnO4}\) 溶液
+            \[\ce{5H2C=CH2 + 2MnO4- + 2H2O + 6H+ -> 5CH2(OH)-CH2(OH) + 2Mn2+}\]
+            3. 热浓酸性 \(\ce{KMnO4}\) 溶液
+            \[\ce{H2C=CH2 ->[\ce{MnO4-/H+}][\Delta] CO2 + H2O}\]
+            Not done yet
+        `
+    },
+    "4-2": {
+        title: String.raw`炔烃 加成 \(\ce{Br2/CCl4}\)`,
+        content: String.raw`\(\ce{H3CCH#CH2 + 2Br2 ->[\ce{CCl4}] H3CCBr2CHBr2}\)`
+    }
+}
