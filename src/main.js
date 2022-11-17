ad1abb8f5b3a55cf7ebdda784720a2ed6f883942
veehz <veehzhz@gmail.com>
2022-11-17 17:27:57 +0800

    fix: hidden backToTop button

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ window.addEventListener("DOMContentLoaded", function () {
     backToTop.id = "back-to-top";
     backToTop.innerHTML =
       '<svg viewBox="0 0 24 24"><path d="M7.41 15.41L12 10.83l4.59 4.58L18 14l-6-6-6 6z"></path></svg>';
+    backToTop.classList.add("hidden");
     document.body.appendChild(backToTop);
     var scrollTrigger = 100;
     window.addEventListener("scroll", function () {
